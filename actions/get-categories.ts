9cea6e94cdab58105d2318899b29c534f81e4693
saifmalik2004 <saifmalik7827@gmail.com>
2024-08-21 16:44:24 +0530

    fix error

--- actions/get-categories.ts
+++ actions/get-categories.ts
@@ -3,7 +3,7 @@ import { Category } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-  console.log(URL);
+  
   const res = await fetch(URL, {
     cache: "no-store", // This will ensure that the data is fetched fresh every time
   });
